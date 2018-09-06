2c342c0c3b9cfb438e692fca8576623c91238db5
wjallison <38045135+wjallison@users.noreply.github.com>
2018-09-06 16:37:19 -0700

    Initial commit.  Map building, saving, and loading are all done.

---
+++ MemoryTest_1/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.IO;
+
+namespace MemoryTest_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Init the map
+            TileMap m = new TileMap();
+
+            //Ask if the user wants to build a map
+            Console.WriteLine("Do you want to build a map? (y/n)");
+            if(Console.ReadKey().KeyChar == 'y')
+            {
+                m = BuildMap(m);
+                SaveMap(m);
+            }
+            else
+            {
+                m = LoadMap();
+            }
+            
+
+            Console.ReadKey();
+        }
+
+        public static TileMap BuildMap(TileMap m)
+        {
+            //Allow the user to build the map
+            int xBuild = 0;
+            int yBuild = 0;
+            bool t = true;
+            Console.Write(m.buildingMap.ToString());
+            while (t)
+            {
+                ConsoleKeyInfo c = Console.ReadKey();
+                switch (c.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        yBuild--;
+                        m.buildingMap = Matrix.Build.DenseOfMatrix(m.map);
+                        //m.buildingMap = m.map;
+                        m.buildingMap[yBuild, xBuild] = 5;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        yBuild++;
+                        m.buildingMap = Matrix.Build.DenseOfMatrix(m.map);
+                        m.buildingMap[yBuild, xBuild] = 5;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        xBuild++;
+                        m.buildingMap = Matrix.Build.DenseOfMatrix(m.map);
+                        m.buildingMap[yBuild, xBuild] = 5;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        xBuild--;
+                        m.buildingMap = Matrix.Build.DenseOfMatrix(m.map);
+                        m.buildingMap[yBuild, xBuild] = 5;
+                        break;
+                    case ConsoleKey.NumPad0:
+                        m.map[yBuild, xBuild] = 1;
+                        break;
+                    case ConsoleKey.NumPad5:
+                        m.map[yBuild, xBuild] = 6;
+                        break;
+                    case ConsoleKey.NumPad8:
+                        m.map[yBuild, xBuild] = 8;
+                        break;
+                    case ConsoleKey.Escape:
+                        t = false;
+                        break;
+                }
+                Console.Write(m.buildingMap.ToString());
+            }
+            Console.Write(m.map.ToString());
+            return m;
+        }
+
+        public static void SaveMap(TileMap m)
+        {
+            Console.WriteLine("Please enter a title:");
+            string name = Console.ReadLine();
+            var csv = new StringBuilder();
+            string line;
+            for(int i = 0; i < 12; i++)
+            {
+                line = m.map[i, 0].ToString();
+                for (int j = 1; j < 12; j++)
+                {
+                    line = line + "," + m.map[i, j].ToString();
+                }
+                csv.AppendLine(line);
+            }
+
+            File.WriteAllText(Directory.GetCurrentDirectory() + @"/" + name + ".csv", csv.ToString());
+
+
+        }
+        public static TileMap LoadMap()
+        {
+            TileMap m = new TileMap();
+
+            DirectoryInfo dInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] files = dInfo.GetFiles("*.csv");
+
+            Console.WriteLine("Please choose the map you wish to load.");
+            for(int i = 0; i < files.Length; i++) 
+            {
+                Console.WriteLine(i.ToString() + ":  " + files[i].Name);
+            }
+            int ind = Convert.ToInt16(Console.ReadLine());
+            if (ind < files.Length)
+            {
+                using(var reader = new StreamReader(files[ind].FullName))
+                {
+                    int i = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        for(int j = 0; j < 12; j++)
+                        {
+                            m.map[i, j] = Convert.ToDouble(values[j]);
+                        }
+                    }
+                }
+            }
+            return m;
+        }
+    }
+}
---
+++ MemoryTest_1/TileMap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MemoryTest_1
+{
+    public class TileMap
+    {
+        public Matrix<double> map;
+
+        public Matrix<double> visibleMap;
+
+        public Matrix<double> buildingMap;
+
+
+        public TileMap()
+        {
+            map = Matrix.Build.Dense(12,12);
+            for(int i = 0; i < 12; i++)
+            {
+                for(int j = 0; j < 12; j++)
+                {
+                    if(i == 0 || i == 11) { map[i, j] = 8; }
+                    if(j == 0 || j == 11) { map[i, j] = 8; }
+                }
+            }
+
+
+
+
+            buildingMap = map;
+            visibleMap = map;
+        }
+
+        public void updateVisibleMap(int x, int y)
+        {
+            visibleMap = Matrix.Build.Dense(12, 12);
+            for (int i = 0; i < 360; i++)
+            {
+                for(int j = 0; j < 20; j++)
+                {
+                    int xP = x + j * Convert.ToInt16(Math.Cos(Convert.ToDouble(i) * Math.PI / 180));
+                    int yP = y + j * Convert.ToInt16(Math.Sin(Convert.ToDouble(i) * Math.PI / 180));
+
+                    if(map[xP,yP] != 8)
+                    {
+                        visibleMap[xP, yP] = map[xP, yP];
+                    }
+                    else
+                    {
+                        visibleMap[xP, yP] = map[xP, yP];
+                        break;
+                    }
+                }
+            }
+
+            visibleMap[x, y] = 1;
+        }
+    }
+}
